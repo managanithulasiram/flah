4572164455f89a89c11907356875561116864615
Thulasiram M <Thulasiram M@ThulasiramM>
2018-08-12 10:25:52 +0530

    refs commit

--- Springone/src/com/springdemo/HappyFortuneService.java
+++ Springone/src/com/springdemo/HappyFortuneService.java
@@ -5,6 +5,8 @@ public class HappyFortuneService implements FortuneService {
 	@Override
 	public String getFortune() {
 		// TODO Auto-generated method stub
+		
+		
 		return "To day is lucky day";
 	}
 
